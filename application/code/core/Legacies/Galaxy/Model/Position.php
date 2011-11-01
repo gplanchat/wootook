3d0f0393f17d4e1d6493d34a66a5ddef929fad03
Orkin <Orkin@PC-ORKIN.home>
2011-11-01 11:32:46 +0100

    Première version de la classe Legacies_Galaxy_Model_Position

--- application/code/core/Legacies/Galaxy/Model/Position.php
+++ application/code/core/Legacies/Galaxy/Model/Position.php
@@ -10,83 +10,101 @@ class Legacies_Galaxy_Model_Position extends Wootook_Core_Entity_SubTable {
 
 	protected $_eventPrefix = 'galaxy.position';
 	protected $_eventObject = 'position';
+
+	/**
+	 * @var Wootook_Empire_Model_Planet
+	 */
+	protected $_moon = null;
+
+	/**
+	 * @var Wootook_Empire_Model_Planet
+	 */
+	protected $_planet = null;
 	
-	protected $_galaxy = null;
-	
-	protected $_system = null;
-	
-	protected $_position = null;
-	
+	/**
+	 * @var Wootook_Empire_Model_User
+	 */
+	protected $_user = null;
 
 	/**
 	 * @var array
 	 */
 	protected static $_instances = array();
 
-	public static function factory($id)
+	/**
+	 *
+	 * Retourne l'instance correspondant à $key
+	 * @param string $key
+	 */
+	public static function factory($key)
 	{
-		if ($id === null) {
-			return new self();
-		}
-
-		$id = intval($id);
-		if (!isset(self::$_instances[$id])) {
+		if (!isset(self::$_instances[$key])) {
 			$instance = new self();
 			$params = func_get_args();
 			call_user_func_array(array($instance, 'load'), $params);
-			self::$_instances[$id] = $instance;
+			self::$_instances[$key] = $instance;
 		}
-		return self::$_instances[$id];
+		return self::$_instances[$key];
 	}
 
+	/**
+	 *
+	 * retourne l'objet présent aux coordonnées données
+	 * @param string/int $galaxy
+	 * @param string/int $system
+	 * @param string/int $position
+	 */
 	public static function factoryFromCoords($galaxy, $system, $position)
 	{
-		if ($coords === null || empty($coords)) {
+
+		if ($galaxy === null || $system === null || $position === null ) {
 			return new self();
 		}
 
-		if (!in_array($type, array(self::TYPE_PLANET, self::TYPE_DEBRIS, self::TYPE_MOON))) {
-			$type = self::TYPE_PLANET;
-		}
+		$galaxy = intval($galaxy);
+		$system = intval($system);
+		$position = intval($position);
 
-		if (!is_array($coords)) {
-			$coords = explode(':', $coords);
-
-			if (count($coords) != 3) {
-				return new self();
-			}
-
-			$coords = array(
-                'galaxy'   => $coords[0],
-                'system'   => $coords[1],
-                'position' => $coords[2],
-                'type'     => $type
-			);
-		}
-
-		if (!isset($coords['type'])) {
-			$coords['type'] = $type;
-		}
+		$coords = array(
+                'galaxy'   => $galaxy,
+                'system'   => $system,
+                'position' => $position
+		);
 
 		$database = Wootook_Database::getSingleton();
-		$collection = new Wootook_Core_Collection('planets');
+		$collection = new Wootook_Core_Collection('galaxy');
 		$collection
-		->column('id')
 		->where('galaxy=:galaxy')
 		->where('system=:system')
 		->where('planet=:position')
-		->where('planet_type=:type')
 		->limit(1)
-		->load($coords)
-		;
+		->load($coords);
+			
+		$galaxyData = $collection->getFirstItem();
 
-		$planetData = $collection->getFirstItem();
-
-		if ($planetData === null || !$planetData->getData('id')) {
+		if ($galaxyData === null || !$galaxyData->getData('galaxy')) {
 			return new self();
 		}
 
-		return self::factory($planetData->getData('id'));
+		// Format la clé de $_instances
+		$key = sprintf('%d:%d:%d', $galaxy, $system, $position);
+
+		return self::factory($key);
+	}
+
+	protected function _init()
+	{
+		$this->_tableName = 'galaxy';
+		$this->_idFieldNames = array('galaxy', 'system', 'planet');
+	}
+	
+		/**
+	 *
+	 * Vérifie si une planete existe pour les coordonnées
+	 * @return bool
+	 */
+	public function hasPlanet() {
+		return (bool) ($this->getData('id_planet'));
 	}
 
 	/**
@@ -95,7 +113,23 @@ class Legacies_Galaxy_Model_Position extends Wootook_Core_Entity_SubTable {
 	 * @return Wootook_Empire_Model_Planet
 	 */
 	public function getPlanet() {
-
+		
+		// On vérifie que qu'une planete existe bien sur l'instance
+		if (!$this->hasPlanet()) {
+			return null;
+		}
+		
+		// On vérifie que la planete n'a pas déjà été chargée
+		if ($this->_planet === null) {
+			
+			// On récupère la planete en fonction de son id
+			$planet = new Wootook_Empire_Model_Planet();
+			$planet->factory($this->getData('id_planet'));
+			
+			$this->_planet = $planet;
+		}
+		
+		return $this->_planet;
 	}
 
 	/**
@@ -105,6 +139,22 @@ class Legacies_Galaxy_Model_Position extends Wootook_Core_Entity_SubTable {
 	 */
 	public function getMoon() {
 
+		// On vérifie s'il y a bien une planete sur l'instance
+		if ($this->getPlanet() === null) {
+			return null;
+		}
+
+		// On récupère la lune de la planete
+		$moon = $this->getPlanet()->getMoon();
+		
+		// TODO je ne suis pas sûr que getMoon() retourne null donc par sécurité
+		if ($moon === null) {
+			return null;
+		}
+				
+		$this->_moon = $moon;
+		
+		return $this->_moon;
 	}
 
 	/**
@@ -114,6 +164,17 @@ class Legacies_Galaxy_Model_Position extends Wootook_Core_Entity_SubTable {
 	 */
 	public function getUser() {
 
+		// On vérifie s'il y a bien une planete sur l'instance
+		if ($this->getPlanet() === null) {
+			return null;
+		}
+		
+		// On va récupérer l'utilisateur à qui appartient la planete (ne peux pas être null puisqu'une planete appartient forcément à un utilisateur)
+		$user = $this->getPlanet()->getUser();
+		
+		$this->_user = $user;
+		
+		return $this->_user;
 	}
 
 	/**
@@ -124,37 +185,95 @@ class Legacies_Galaxy_Model_Position extends Wootook_Core_Entity_SubTable {
 	public function getAlliance() {
 
 	}
-	
+
+	/**
+	 *
+	 * Retourne la galaxy
+	 */
 	public function getGalaxy() {
-		return $this->_galaxy;
+		return (int) $this->getData('galaxy');
 	}
-	
+
+	/**
+	 * Modifie la galaxy
+	 * @param string/int $galaxy
+	 */
 	public function setGalaxy($galaxy) {
-		$this->_galaxy = $galaxy;
+		$this->setData('galaxy', $galaxy);
+
+		return $this;
 	}
-	
+
+	/**
+	 *
+	 * Retourne le system
+	 */
 	public function getSystem() {
-		return $this->_system;
+		return (int) $this->getData('system');
 	}
-	
+
+	/**
+	 *
+	 * Modifie le system
+	 * @param string/int $system
+	 */
 	public function setSystem($system) {
-		$this->_system = $system;
+		$this->setData('system', $system);
+
+		return $this;
 	}
-	
+
+	/**
+	 *
+	 * Retourne la position
+	 */
 	public function getPosition() {
-		return $this->_position;
+		return (int) $this->getData('planet');
 	}
-	
+
+	/**
+	 *
+	 * Modifie la position
+	 * @param string/int $position
+	 */
 	public function setPosition($position) {
-		$this->_position = $position;
+		$this->setData('planet', $position);
+
+		return $this;
 	}
-	
+
+	/**
+	 *
+	 * Retourne le débris lié au type passé en paramètre
+	 * @param string de type Legacies_Empire::RESOURCE_* $resourceType
+	 */
 	public function getDebrisAmount($resourceType) {
+		$resource = Wootook_Empire_Model_Game_Resources::getSingleton();
+		$resourceConfig = $resource->getData($resourceType);
 		
+		if ($resourceConfig === null) {
+			return null;
+		}
+		
+		return (int) $this->getData($resourceConfig['field']);
 	}
-	
+
+	/**
+	 *
+	 * Modifie la valeur du champ en fonction du type de retource passé en paramètre
+	 * @param string de type Legacies_Empire::RESOURCE_* $resourceType
+	 * @param string/int $amount
+	 */
 	public function setDebrisAmount($resourceType, $amount) {
+		$resource = Wootook_Empire_Model_Game_Resources::getSingleton();
+		$resourceConfig = $resource->getData($resourceType);
 		
+		if ($resourceConfig === null) {
+			return null;
+		}
+		$this->setData($resourceConfig('field'), $amount);
+
+		return $this;
 	}
 
 }
